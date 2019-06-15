8cc36edd524f0d77ca910547cae4fa68c4e6b4e1
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-06-15 22:37:03 +0200

    - Created Frontend + Backend to show SimpleDreamer Backends

---
+++ SimpleDreamerVS19/Frontend_To_Show_SimpleDreamerBackends/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend_To_Show_SimpleDreamerBackends.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
