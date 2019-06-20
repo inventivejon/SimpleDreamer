f2d1a08823a31bca704e889d2e56aa49f503733c
inventivejon <jonathan.daniel.herrmann@gmail.com>
2019-06-20 09:24:59 +0200

    Finished work on docker swarm run locally example

--- SimpleDreamerVS19/docker-swarm_usage_examples/ServiceToShowSimpleDreamerBackends/StartServiceToShowSimpleDreamerBackends.sh
+++ SimpleDreamerVS19/docker-swarm_usage_examples/ServiceToShowSimpleDreamerBackends/StartServiceToShowSimpleDreamerBackends.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build --tag simpledreamer_sb_backend:v0.1 ./../../Backend_To_Show_SimpleDreamerBackends
-docker tag simpledreamer_sb_backend:v0.1 localhost:5000/simpledreamer_sb_backend:v0.1
-docker push localhost:5000/simpledreamer_sb_backend:v0.1
+docker tag simpledreamer_sb_backend:v0.1 127.0.0.1:5000/simpledreamer_sb_backend:v0.1
+docker push 127.0.0.1:5000/simpledreamer_sb_backend:v0.1
 docker build --tag simpledreamer_sb_frontend:v0.1 ./../../Frontend_To_Show_SimpleDreamerBackends
-docker tag simpledreamer_sb_frontend:v0.1 localhost:5000/simpledreamer_sb_frontend:v0.1
-docker push localhost:5000/simpledreamer_sb_frontend:v0.1
+docker tag simpledreamer_sb_frontend:v0.1 127.0.0.1:5000/simpledreamer_sb_frontend:v0.1
+docker push 127.0.0.1:5000/simpledreamer_sb_frontend:v0.1
 docker stack deploy --compose-file=ShowSimpleDreamerBackendsService.yml SimpleDreamer_SB
