684b9f0abd20c5170d929a536d38da9612c8296b
inventivejon <jonathan.daniel.herrmann@gmail.com>
2019-06-23 12:22:32 +0200

    Updated docker-swarm file for Blazor

--- docker-swarm_usage_examples/Blazor_ServiceToShowSimpleDreamerBackends/StartServiceToShowSimpleDreamerBackends.sh
+++ docker-swarm_usage_examples/Blazor_ServiceToShowSimpleDreamerBackends/StartServiceToShowSimpleDreamerBackends.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-docker build --tag simpledreamer_sb_backend:v0.1 ./../../SimpleDreamerVS19/Backend_To_Show_SimpleDreamerBackends
-docker tag simpledreamer_sb_backend:v0.1 127.0.0.1:5000/simpledreamer_sb_backend:v0.1
-docker push 127.0.0.1:5000/simpledreamer_sb_backend:v0.1
-docker build --tag simpledreamer_sb_blazor_frontend:v0.1 -f ./../../SimpleDreamerVS19/Blazor_Dockerfile ./../../SimpleDreamerVS19
-docker tag simpledreamer_sb_blazor_frontend:v0.1 127.0.0.1:5000/simpledreamer_sb_blazor_frontend:v0.1
-docker push 127.0.0.1:5000/simpledreamer_sb_blazor_frontend:v0.1
+docker build --tag simpledreamer_sb_blazor:v0.1 -f ./../../SimpleDreamerVS19/Blazor_Dockerfile ./../../SimpleDreamerVS19
+docker tag simpledreamer_sb_blazor:v0.1 127.0.0.1:5000/simpledreamer_sb_blazor:v0.1
+docker push 127.0.0.1:5000/simpledreamer_sb_blazor:v0.1
 docker stack deploy --compose-file=ShowSimpleDreamerBackendsService.yml SimpleDreamer_SB
