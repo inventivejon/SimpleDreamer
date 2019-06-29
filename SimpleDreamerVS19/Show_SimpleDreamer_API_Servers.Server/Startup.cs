6a2fc3a863c7826ef590c21d3b79db6efecb1a6c
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-06-29 14:30:04 +0200

    - First test to switch between client side and server side

--- SimpleDreamerVS19/Show_SimpleDreamer_API_Servers.Server/Startup.cs
+++ SimpleDreamerVS19/Show_SimpleDreamer_API_Servers.Server/Startup.cs
@@ -2,14 +2,17 @@ using Blazor.FlexGrid;
 using Blazor.FlexGrid.DataSet;
 using Blazor.FlexGrid.DataSet.Http;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
+using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 
 namespace Show_SimpleDreamer_API_Servers.Server
 {
@@ -20,13 +23,28 @@ namespace Show_SimpleDreamer_API_Servers.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddNewtonsoftJson();
+            services.AddServerSideBlazor();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
                     new[] { "application/octet-stream" });
             });
-            
             services.AddSingleton<StaticRepositoryCollections>();
+
+            // Server Side Blazor doesn't register HttpClient by default
+            if (!services.Any(x => x.ServiceType == typeof(HttpClient)))
+            {
+                // Setup HttpClient for server side in a client side compatible fashion
+                services.AddScoped<HttpClient>(s =>
+                {
+                    // Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+                    var uriHelper = s.GetRequiredService<IUriHelper>();
+                    return new HttpClient
+                    {
+                        BaseAddress = new Uri(uriHelper.GetBaseUri())
+                    };
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +64,7 @@ namespace Show_SimpleDreamer_API_Servers.Server
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapBlazorHub<Client.App>("app");
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
             });
